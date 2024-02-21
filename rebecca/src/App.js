f9b354c9e2c34b98f2a299820de56c4ab2ecdd35
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2024-02-21 16:07:13 -0500

    Favicon and side nav set up

--- rebecca/src/App.js
+++ rebecca/src/App.js
@@ -2,17 +2,19 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Homepage from "./Homepage";
 import Contact from "./Contact";
+import Header from "./Header";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
---
+++ rebecca/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container header">
+      <div className="header-contents">
+        <a className="header-name" href="/">
+          Rebecca Storm
+        </a>
+        <a href="/collection">Collection</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
