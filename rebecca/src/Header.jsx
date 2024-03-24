271fe0e7a02ceb85b22dac85ea54413b9aee947e
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2024-03-24 15:44:11 -0400

    minor adjustments and mobile nav

--- rebecca/src/Header.jsx
+++ rebecca/src/Header.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import React from "react";
+import HamburgerNav from "./assets/HamburgerNav";
 
 const Header = () => {
+  const [mobileNav, setMobileNav] = useState(false);
+
   return (
     <div className="container header">
       <div className="header-contents">
         <a className="header-link name bold" href="/">
           Rebecca Storm
         </a>
-        <a className="header-link" href="/collection">
-          Collection
-        </a>
-        <a className="header-link" href="/contact">
-          Contact
-        </a>
+        <button className="hamburger-icon">
+          <HamburgerNav />
+        </button>
+        <div className="header-list">
+          <a className="header-link" href="/collection">
+            Collection
+          </a>
+          <a className="header-link" href="/contact">
+            Contact
+          </a>
+        </div>
       </div>
     </div>
   );
